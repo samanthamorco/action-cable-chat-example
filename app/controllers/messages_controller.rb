5d9a720f9a61b56087a94e97090979097554288b
Samantha Morco <samanthamorco@gmail.com>
2017-06-29 16:24:40 -0400

    split message into its own partial

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,15 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
   def create
-    @message = Message.create(content: params[:content], user: current_user, room_id: params[:room_id])
-    redirect_to room_path(params[:room_id])
+    @message = Message.new(content: params[:content], user: current_user, room_id: params[:room_id])
+    if @message.save
+      ActionCable.server.broadcast 'messages',
+        message: message.content,
+        user: message.user.email
+      head :ok
+    else
+      flash[:warning] = "Message not added"
+    end
+      redirect_to room_path(params[:room_id])
   end
 end
